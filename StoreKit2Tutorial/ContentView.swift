deca42367c60b2fc9bc27702bc3af16bfc6471ac
Thompson Dean <dean.thompson@arsaga.jp>
2023-11-07 14:14:26 +0900

    Initial Commit

---
+++ StoreKit2Tutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StoreKit2Tutorial
+//
+//  Created by Thompson Dean on 2023/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoreKit2Tutorial/StoreKit2TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit2TutorialApp.swift
+//  StoreKit2Tutorial
+//
+//  Created by Thompson Dean on 2023/11/07.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit2TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
