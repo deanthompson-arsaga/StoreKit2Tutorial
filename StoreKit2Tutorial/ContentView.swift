d5c548be29f17831a31391b5e367de8f159e17cf
Thompson Dean <dean.thompson@arsaga.jp>
2023-11-08 12:56:23 +0900

    test

--- StoreKit2Tutorial/ContentView.swift
+++ StoreKit2Tutorial/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var storeKitManager = StoreKitManager()
-    
-    let imageNameArray: [String] = ["akaishi_pet", "ando", "asakura", "tomoaki", "uchida", "ultra", "yoshida", "dean"]
-    
+    @State var isShowingAlert: Bool = false
     let columns: [GridItem] = [.init(.flexible()), .init(.flexible())]
     
     var body: some View {
@@ -20,32 +18,60 @@ struct ContentView: View {
             GeometryReader { geo in
                 ScrollView {
                     LazyVGrid(columns: columns) {
-                        ForEach(imageNameArray, id: \.self) { imageString in
+                        ForEach(storeKitManager.items, id: \.self) { product in
                             VStack {
-                                NavigationLink {
-                                    DetailView(imageString: imageString)
-                                } label: {
-                                    Image(imageString)
+                                if storeKitManager.isProductPurchased(product.id) {
+                                    NavigationLink {
+                                        DetailView(imageString: product.id)
+                                    } label: {
+                                        Image(product.id)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: geo.size.width / 2 - 20, height: geo.size.width / 2 - 20)
+                                            .clipped()
+                                            .cornerRadius(8)
+                                    }
+                                } else {
+                                    Image(product.id)
                                         .resizable()
                                         .scaledToFill()
                                         .frame(width: geo.size.width / 2 - 20, height: geo.size.width / 2 - 20)
                                         .clipped()
                                         .cornerRadius(8)
+                                        .onTapGesture {
+                                            storeKitManager.selectProductForPurchase(product)
+                                            isShowingAlert = true
+                                        }
                                 }
+                                
                                 Button {
-                                    print(imageString)
+                                    Task {
+                                        await storeKitManager.purchase(product)
+                                    }
                                 } label: {
-                                    Text("Buy Now")
+                                    Text(storeKitManager.isProductPurchased(product.id) ? "Purchased" : "Buy Now \(product.displayPrice)")
                                         .padding(8)
                                         .frame(width: geo.size.width / 2 - 20)
                                         .foregroundColor(.white)
-                                        .background(.blue)
+                                        .background(storeKitManager.isProductPurchased(product.id) ? .green : .blue)
                                         .cornerRadius(8)
                                 }
                             }
                         }
                     }
                     .padding(12)
+                    .alert(isPresented: $isShowingAlert) {
+                        Alert(
+                            title: Text("Purchase needed"),
+                            message: Text("Would you like to buy \(storeKitManager.selectedProductForPurchase?.displayPrice ?? "") to view this picture?"),
+                            primaryButton: .default(Text("Buy Now")) {
+                                Task {
+                                    await storeKitManager.purchaseSelectedProduct()
+                                }
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    }
                 }
             }
             .navigationTitle("App Div")
--- StoreKit2Tutorial/StoreKitManager.swift
+++ StoreKit2Tutorial/StoreKitManager.swift
@@ -6,9 +6,150 @@
 //
 
 import Foundation
+import SwiftUI
 import StoreKit
+
+enum PaymentState {
+    case successful
+    case pending
+    case failed
+    case userCancelled
+}
+
+typealias TransactionListener = Task<Void, Error>
+typealias PurchaseResult = Product.PurchaseResult
 
 @MainActor
 final class StoreKitManager: ObservableObject {
+    let storeKitIdentifiers: [String] = ["akaishi_pet", "ando", "asakura", "tomoaki", "uchida", "ultra", "yoshida"]
+    @Published private(set) var items: [Product] = []
+    @Published private(set) var purchasedProductIDs: Set<String> = []
+    @Published var selectedProductForPurchase: Product?
+    @Published private(set) var paymentState: PaymentState? {
+        didSet {
+            switch paymentState {
+            case .failed:
+                hasError = true
+            default:
+                hasError = false
+            }
+        }
+    }
     
+    @Published var hasError: Bool = false
+    
+    private var transactionListener: TransactionListener?
+       
+       init() {
+           transactionListener = configureTransactionListener()
+        Task { [weak self] in
+            await self?.retrieveProducts()
+            await self?.refreshPurchasedProducts()
+        }
+    }
+    
+    deinit {
+        transactionListener?.cancel()
+    }
+    
+    func purchase(_ item: Product) async {
+        do {
+            let result = try await item.purchase()
+            
+            try await handlePurchase(from: result)
+        } catch {
+            paymentState = .failed
+            print("DEBUG: \(error.localizedDescription)")
+        }
+    }
+    
+    func reset() {
+        paymentState = nil
+    }
+    
+    func isProductPurchased(_ productID: String) -> Bool {
+        return purchasedProductIDs.contains(productID)
+    }
+    
+    func selectProductForPurchase(_ product: Product) {
+        selectedProductForPurchase = product
+    }
+    
+    func purchaseSelectedProduct() async {
+        if let product = selectedProductForPurchase {
+            await purchase(product)
+            selectedProductForPurchase = nil
+        }
+    }
 }
+
+extension StoreKitManager {
+    func configureTransactionListener() -> TransactionListener {
+            Task.detached(priority: .background) { @MainActor [weak self] in
+                do {
+                    for await result in Transaction.updates {
+                        let transaction = try self?.checkVerified(result)
+                        self?.paymentState = .successful
+                        await transaction?.finish()
+                    }
+                } catch {
+                    self?.paymentState = .failed
+                    print(error)
+                }
+            }
+        }
+    
+    func refreshPurchasedProducts() async {
+        for await result in Transaction.currentEntitlements {
+            switch result {
+            case .verified(let transaction):
+                if transaction.revocationDate == nil {
+                    purchasedProductIDs.insert(transaction.productID)
+                }
+                await transaction.finish()
+            case .unverified(_, _):
+                break
+            }
+        }
+    }
+    
+    func retrieveProducts() async {
+        do {
+            let products = try await Product.products(for: storeKitIdentifiers).sorted(by: { $0.price < $1.price})
+            items = products
+            for item in items {
+                print(item.displayName)
+            }
+        } catch {
+            paymentState = .failed
+            print("DEBUG: \(error.localizedDescription)")
+        }
+    }
+    
+    func handlePurchase(from result: PurchaseResult) async throws {
+        switch result {
+        case .success(let verification):
+            let transaction = try checkVerified(verification)
+            paymentState = .successful
+            purchasedProductIDs.insert(transaction.productID)
+            await transaction.finish()
+        case .pending:
+            paymentState = .pending
+        case .userCancelled:
+            paymentState = .userCancelled
+        @unknown default:
+            break
+        }
+    }
+    
+    func checkVerified<T>(_ result: VerificationResult<T>) throws -> T {
+        switch result {
+        case .unverified(_, _):
+            print("The verfication of the user failed.")
+            throw URLError(.cancelled)
+        case .verified(let safe):
+            return safe
+        }
+    }
+}
+
